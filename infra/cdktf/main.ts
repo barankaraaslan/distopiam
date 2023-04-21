7ab93f9203abbed3cf719df5778458ea7f223c9c
Baran Karaaslan <barankaraaslan@protonmail.com>
2023-04-22 00:37:06 +0300

    create TerraformBackend stack and use its s3 as backend

--- infra/cdktf/main.ts
+++ infra/cdktf/main.ts
@@ -1,14 +1,29 @@
 import { Construct } from "constructs";
-import { App, TerraformStack } from "cdktf";
+import { App, S3Backend, TerraformStack } from "cdktf";
 import * as aws from "@cdktf/provider-aws";
 
-class UserManagment extends TerraformStack {
+class TerraformBackend extends TerraformStack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
     new aws.provider.AwsProvider(this, "provider");
+
+    const callerId = new aws.dataAwsCallerIdentity.DataAwsCallerIdentity(
+      this,
+      "current"
+    );
+    new aws.s3Bucket.S3Bucket(this, "terraform-backend", {
+      bucket: `terraform-backend-${callerId.accountId}`,
+    });
+
+    new S3Backend(this, {
+      bucket: "terraform-backend-322343128595",
+      key: id,
+      region: "eu-west-1",
+    });
   }
 }
 
 const app = new App();
-new UserManagment(app, "UserManagment");
+new TerraformBackend(app, "TerraformBackend");
+
 app.synth();
